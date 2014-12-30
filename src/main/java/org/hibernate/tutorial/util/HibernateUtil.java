a5ca1be6fd71af86c3de64b3230b21ac983b2146
Qinghui Xu <qinghui.xu@kleegroup.com>
2014-12-30 10:32:11 +0100

    bug fixed: configuration settings are necessary for ServiceRegistry to be built

--- src/main/java/org/hibernate/tutorial/util/HibernateUtil.java
+++ src/main/java/org/hibernate/tutorial/util/HibernateUtil.java
@@ -14,7 +14,9 @@ public class HibernateUtil {
     private static SessionFactory buildSessionFactory() {
         try {
             // Create the SessionFactory from hibernate.cfg.xml
-            return new Configuration().configure().buildSessionFactory(new StandardServiceRegistryBuilder().build());
+            Configuration configuration = new Configuration().configure();
+            return configuration.buildSessionFactory(new StandardServiceRegistryBuilder().applySettings(configuration.getProperties())
+                    .build());
         } catch (Throwable ex) {
             // Make sure you log the exception, as it might be swallowed
             System.err.println("Initial SessionFactory creation failed." + ex);
