a0c098233fd425ba34e61135102aeceaffef498b
Qinghui Xu <qinghui.xu@kleegroup.com>
2015-01-02 16:28:47 +0100

    Person in association with Event

---
+++ src/main/java/org/hibernate/tutorial/domain/Person.java
@@ -0,0 +1,97 @@
+package org.hibernate.tutorial.domain;
+
+import java.util.Set;
+
+/**
+ * Type «Person».
+ */
+public class Person {
+
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private int age;
+
+    private Set<Event> events;
+
+    /**
+     * @return «id» value.
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Set «id».
+     *
+     * @param id new value of «id».
+     */
+    private void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return «firstname» value.
+     */
+    public String getFirstname() {
+        return firstname;
+    }
+
+    /**
+     * Set «firstname».
+     *
+     * @param firstname new value of «firstname».
+     */
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    /**
+     * @return «lastname» value.
+     */
+    public String getLastname() {
+        return lastname;
+    }
+
+    /**
+     * Set «lastname».
+     *
+     * @param lastname new value of «lastname».
+     */
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    /**
+     * @return «age» value.
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * Set «age».
+     *
+     * @param age new value of «age».
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * @return «events» value.
+     */
+    public Set<Event> getEvents() {
+        return events;
+    }
+
+    /**
+     * Set «events».
+     *
+     * @param events new value of «events».
+     */
+    public void setEvents(Set<Event> events) {
+        this.events = events;
+    }
+
+}
