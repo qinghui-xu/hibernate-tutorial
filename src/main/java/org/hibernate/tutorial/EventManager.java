7ee3cfc52e3cb8ac176ce3728df53c546117faa8
Qinghui Xu <qinghui.xu@kleegroup.com>
2015-01-02 17:46:24 +0100

    association test

--- src/main/java/org/hibernate/tutorial/EventManager.java
+++ src/main/java/org/hibernate/tutorial/EventManager.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.tutorial.domain.Event;
+import org.hibernate.tutorial.domain.Person;
 import org.hibernate.tutorial.util.HibernateUtil;
 
 /**
@@ -23,29 +24,86 @@ public class EventManager {
                 Event theEvent = events.get(i);
                 System.out.println("Event: " + theEvent.getTitle() + " Time: " + theEvent.getDate());
             }
+        } else if (args[0].equals("addpersontoevent")) {
+            Long eventId = mgr.createAndStoreEvent("Event to associate", new Date());
+            Long personId = mgr.createAndStorePerson("Foo", "Bar", 18);
+            mgr.addPersonToEvent(personId, eventId);
+            System.out.println("Added person " + personId + " to event " + eventId);
         }
 
         HibernateUtil.getSessionFactory().close();
     }
 
-    private void createAndStoreEvent(String title, Date theDate) {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
+    private Long createAndStoreEvent(String title, Date theDate) {
+        Event event = newEvent(title, theDate);
 
-        Event theEvent = new Event();
-        theEvent.setTitle(title);
-        theEvent.setDate(theDate);
-        session.save(theEvent);
+        Session session = openSession();
+        try {
+            Long eventId = (Long) session.save(event);
+            session.getTransaction().commit();
+            return eventId;
+        } finally {
+            closeSession(session);
+        }
+    }
 
-        session.getTransaction().commit();
+    private Event newEvent(String title, Date theDate) {
+        Event event = new Event();
+        event.setTitle(title);
+        event.setDate(theDate);
+        return event;
+    }
+
+    private Long createAndStorePerson(String firstname, String lastname, int age) {
+        Person person = newPerson(firstname, lastname, age);
+
+        Session session = openSession();
+        try {
+            Long personId = (Long) session.save(person);
+            session.getTransaction().commit();
+            return personId;
+        } finally {
+            closeSession(session);
+        }
+    }
+
+    private Person newPerson(String firstname, String lastname, int age) {
+        Person person = new Person();
+        person.setAge(age);
+        person.setFirstname(firstname);
+        person.setLastname(lastname);
+        return person;
+    }
+
+    private void addPersonToEvent(Long personId, Long eventId) {
+        Session session = openSession();
+        try {
+            Person person = (Person) session.load(Person.class, personId);
+            Event event = (Event) session.load(Event.class, eventId);
+            person.getEvents().add(event);
+            session.getTransaction().commit();
+        } finally {
+            closeSession(session);
+        }
     }
 
     private List<Event> listEvents() {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
+        Session session = openSession();
         try {
             return session.createQuery("from Event").list();
         } finally {
+            session.getTransaction().rollback();
+        }
+    }
+
+    private static Session openSession() {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        return session;
+    }
+
+    private static void closeSession(Session session) {
+        if (session.isOpen()) {
             session.getTransaction().rollback();
         }
     }
